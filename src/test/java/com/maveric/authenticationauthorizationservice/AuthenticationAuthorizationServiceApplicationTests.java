85e13e1419a4d5ce3ea9491453463ce1998d5ff3
Hins Jain <hinsj@maveric-systems.com>
2023-03-01 15:39:52 +0530

    Added logback.xml file

--- src/test/java/com/maveric/authenticationauthorizationservice/AuthenticationAuthorizationServiceApplicationTests.java
+++ src/test/java/com/maveric/authenticationauthorizationservice/AuthenticationAuthorizationServiceApplicationTests.java
@@ -3,11 +3,14 @@ package com.maveric.authenticationauthorizationservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class AuthenticationAuthorizationServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue(true);
 	}
 
 }
